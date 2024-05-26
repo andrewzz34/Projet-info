bbae4329a84cd73374db66cace5f98c637d2edde
6prrri1 <132775745+6prrri1@users.noreply.github.com>
2024-05-26 23:33:31 +0200

    Update cyfish.h

--- cyfish.h
+++ cyfish.h
@@ -10,11 +10,34 @@
 typedef struct {
     char player_name[PLAYER_NAME_BUFFER];
     int score;
-    int penguins; 
+    int penguins;
+    int id_player; 
 } Player;
 
-void setupPlayers(Player * players, int num_players);
+typedef struct {
+    int fishes; // Nombre de poissons sur la case
+    int penguin; // Identifiant du pingouin sur la case, 0 si aucun
+    int broken; // Indique si la case est occupée (1 pour oui, 0 pour non)
+    int playeron;
+} Cell;
+
+void setupPlayers(Player* players, int num_players);
 void printHexagonLine(int num_hexagons);
-void printGameBoard(int rows, int cols, Player * players, int num_players);
+void printGameBoard(Cell** board,int rows, int cols, Player* players, int num_players,char** colors,int* fishes);
+void printboard(Cell** board,int rows, int cols);
+Cell** initializeBoard(int rows, int cols);
+void freeBoard(Cell** board, int rows);
+void printfishes(Cell** board,int rows, int cols);
+void affichePenguin(Cell** board, int rows, int cols, int num_players,char** colors,Player* players,int* fishes);
+int verifyT(Cell **board, int crows, int ccols, int rows, int cols);
+int verifyTR(Cell **board, int crows, int ccols, int rows, int cols);
+int verifyTL(Cell **board, int crows, int ccols, int rows, int cols);
+int verifyL(Cell **board, int crows, int ccols, int rows, int cols);
+int verifyLR(Cell **board, int crows, int ccols, int rows, int cols);
+int verifyLL(Cell **board, int crows, int ccols, int rows, int cols);
+int possiblemove(Cell **board, int crows, int ccols, int rows, int cols);
+
+
+
 
 #endif
