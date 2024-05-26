99f593d3a8d9078de9d02678ca04ee9048b23c6b
6prrri1 <132775745+6prrri1@users.noreply.github.com>
2024-05-26 23:27:37 +0200

    Update player.c

--- player.c
+++ player.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include "cyfish.h"
-
-void setupPlayers(Player * players, int num_players) {
+void setupPlayers(Player* players, int num_players) {
     for (int i = 0; i < num_players; i++) {
         printf("Enter name for player %d (up to 49 characters): ", i + 1);
-        scanf("%s", players[i].player_name);
-        while (getchar() != '\n');  
+        scanf("%49s", players[i].player_name); // Limite l'entrée pour éviter le débordement de tampon
+        while (getchar() != '\n');
 
         if (strlen(players[i].player_name) > MAX_NAME_LENGTH) {
             printf("Name entered is too long. Please enter a name up to 49 characters.\n");
-            scanf("%s", players[i].player_name);
+            scanf("%49s", players[i].player_name); // Limite l'entrée pour éviter le débordement de tampon
         }
 
         players[i].score = 0;
+        players[i].id_player = i;
+
+      
 
         if (num_players == 2) {
             players[i].penguins = 4;
